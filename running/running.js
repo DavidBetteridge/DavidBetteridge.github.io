5fc885c32cdd82209ab5712d834786836ed9226e
David Betteridge <david.betteridge@proactis.com>
2019-03-04 22:24:53 +0000

    WIP

--- running/running.js
+++ running/running.js
@@ -1,49 +1,86 @@
 var map
+var layer
 
 function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         /* No need to set credentials if already passed in URL */
         center: new Microsoft.Maps.Location(53.4808, -1.2426),
         mapTypeId: Microsoft.Maps.MapTypeId.aerial,
-        zoom: 7 });
-   
-    displayRoute("/running/data/Gargrave_to_Leeds_along_the_Leeds_Liverpool_Canal.xml", map)
-    displayRoute("/running/data/Leeds_to_York.xml", map)
+        zoom: 7
+    });
+
+    layer = new Microsoft.Maps.Layer();
+    map.layers.insert(layer);
 }
 
-function displayRoute(xmlFile, map) {
+function displayRoute(xmlFile, map, colour) {
 
     function handler() {
 
-        if (this.status == 200 &&
-            this.responseXML != null) {
+        if (this.status == 200 && this.responseXML != null) {
             var xmlDoc = this.responseXML;
             var coords = [];
             var trkpts = xmlDoc.getElementsByTagName("trkpt");
-            for (var i = 0; i < trkpts.length; i++) {
-                var trkpt = trkpts[i];
-                var longitude = trkpt.getAttribute("lon");
-                var latitude = trkpt.getAttribute("lat");
-                coords.push(new Microsoft.Maps.Location(latitude, longitude));
-            }
 
-            if (coords.length > 0) {
+            if (trkpts.length > 0) {
+                for (var i = 0; i < trkpts.length; i++) {
+                    var trkpt = trkpts[i];
+                    var longitude = trkpt.getAttribute("lon");
+                    var latitude = trkpt.getAttribute("lat");
+                    coords.push(new Microsoft.Maps.Location(latitude, longitude));
+                }
+
                 var line = new Microsoft.Maps.Polyline(coords, {
-                    strokeColor: 'blue',
+                    strokeColor: colour,
                     strokeThickness: 3,
-                    // strokeDashArray: [4, 4]
+                    metadata: xmlFile
                 });
-                map.entities.push(line);
+                layer.add(line);
             }
-        } else {
-           // alert("error");
+        } 
+        else 
+        {
+            //console.log("Error getting xml file " + xmlFile + " status code " + this.status);
         }
     }
 
     var client = new XMLHttpRequest();
-    client.onreadystatechange  = handler;
-    client.open("GET", xmlFile);
+    client.onreadystatechange = handler;
+    client.open("GET", "/running/data/" + xmlFile);
     client.send();
 }
 
+function redrawMap() {
+    layer.clear();
+
+    var checkboxes = document.querySelectorAll(".route");
+    checkboxes.forEach(function (checkbox) {
+        if (checkbox.checked) {
+            var filename = checkbox.getAttribute("value");
+            displayRoute(filename, map, 'red')
+        }
+    });
+}
+
+function removeRoute(filename, layer) {
+    var primitives = layer.getPrimitives();
+    primitives.forEach(function (primitive) {
+        if (primitive.metadata == filename) {
+            layer.remove(primitive);
+        }
+    });
+}
+
+
+var checkboxes = document.querySelectorAll(".route");
+checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () {
+        if (this.checked) {
+            var filename = this.getAttribute("value");
+            displayRoute(filename, map, 'red')
+        } else {
+            removeRoute(filename, layer)
+        }
+    });
+});
 
