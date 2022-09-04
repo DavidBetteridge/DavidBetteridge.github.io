ee1a36b6e10cb60922c5f9d787a477ffc7596eaf
David Betteridge <davidabetteridge@gmail.com>
2022-09-04 16:19:15 +0100

    wip

--- build.py
+++ build.py
@@ -1,16 +1,23 @@
 import markdown
 import os
+import shutil
 
-def convert_file(converter: markdown.Markdown, filename: str, template: str):
+def convert_file(converter: markdown.Markdown,
+                 filename: str,
+                 template: str,
+                 source_folder: str,
+                 target_folder: str):
   print(f"Converting {filename}...")
-  with open(f'source_files/{filename}.md', 'r') as f:
-      source = f.read()
-
-  html = converter.convert(source)
-  html = template.replace("{{body}}", html)
-
-  with open(f'target_files/{filename}.html', 'w') as f:
-      f.write(html)
+  try:
+    with open(f'{source_folder}/{filename}.md', 'r') as f:
+        source = f.read()
+    html = converter.convert(source)
+    html = template.replace("{{body}}", html)\
+                  .replace("{{site.baseurl}}", "https://davidbetteridge.net/")
+    with open(f'{target_folder}/{filename}.html', 'w') as f:
+        f.write(html)
+  except Exception as e:
+    print(str(e))
 
 def convert_all_source_files():
   converter = markdown.Markdown()
@@ -19,9 +26,31 @@ def convert_all_source_files():
 
   for filename in os.listdir("source_files"):
     without_ext = os.path.splitext(filename)[0]
-    convert_file(converter, without_ext, template)
+    convert_file(converter, without_ext, template, "source_files", "target_files")
 
-  print("Complete")
+
+def convert_all_blog_posts():
+  converter = markdown.Markdown()
+  with open('template.html', 'r') as f:
+      template = f.read()
+
+  for filename in os.listdir("blog_posts"):
+    without_ext = os.path.splitext(filename)[0]
+    convert_file(converter, without_ext, template, "blog_posts", "target_blog_posts")
+
+def build_blogs_index():
+#   ---
+# layout: post
+# title: How does await work?
+# ---
+  pass
+
+def move_index_html():
+  src_path = "target_files/index.html"
+  dst_path = "index.html"
+  shutil.move(src_path, dst_path)
 
 if __name__ == "__main__":
   convert_all_source_files()
+  move_index_html()
+  convert_all_blog_posts()
--- source_files/running.md
+++ source_files/running.md
@@ -1,6 +1,6 @@
 # Running
 
-In my free time I'm a keen runner and have competed in several ultra-marathons. These include the Winner Spine Race,  the Hardmoors 200 and the Arc of Attrition.
+In my free time I'm a keen runner and have competed in several ultra-marathons. These include the Winter Spine Race,  the Hardmoors 200 and the Arc of Attrition.
 
 Most of my runs can be found on [Strava](https://www.strava.com/athletes/6352224)
 
